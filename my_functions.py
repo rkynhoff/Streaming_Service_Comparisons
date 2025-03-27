9f06e10f590d667cbfece05b9e6de77018787ff7
Kynhoff <rkynhoff@gmail.com>
2025-03-27 13:42:33 -0400

    Functions update to fix a warning code

--- my_functions.py
+++ my_functions.py
@@ -13,8 +13,10 @@ def wrap_xticks(ax, width, break_long_words=False):
         text = label.get_text()
         # Wrap the text to fit within the specified width using the textwrap.fill function and textwrap module, determine if long words should be broken, append wrapped text to the labels list
         labels.append(textwrap.fill(text, width=width, break_long_words=break_long_words))
-    # Set the tick labels on the x-axis to the wrapped text labels and keep the text horizontally oriented
-    ax.set_xticklabels(labels, rotation=0)
+    # Set the x ticks and x tick labels on the x-axis to the wrapped text labels and keep the text horizontally oriented
+    ax.xaxis.set_major_locator(plt.FixedLocator(range(len(labels))))
+    ax.set_xticklabels(labels, rotation = 0, ha = "right")
+    
 
 # Function for adding bar count labels in non-stacked bar graphs in large font size
 # Define the function
@@ -34,7 +36,7 @@ def add_bar_value_counts_md():
         # Obtain the bar height and change it to an integer
         height = int(p.get_height())
         # Add the value count in the specified location (center of the bar) at half the height of the bar, center the font, make it white, in a specific fontsize and bold it
-        plt.gca().text(p.get_x() + p.get_width()/2, height/2, str(height), ha = "center", color = "white", fontsize = 10, fontweight = "bold")
+        plt.gca().text(p.get_x() + p.get_width()/2, height/2, str(height), ha = "center", color = "white", fontsize = 9, fontweight = "bold")
 
 # Function for adding bar count labels in non-stacked bar graphs in small font size
 # Define function
@@ -44,4 +46,29 @@ def add_bar_value_counts_sm():
         # Obtain the bar height and change it to an integer
         height = int(p.get_height())
         # Add the value count in the specified location (center of the bar) at half the height of the bar, center the font, make it white, in a specific fontsize and bold it
-        plt.gca().text(p.get_x() + p.get_width()/2, height/2, str(height), ha = "center", color = "white", fontsize = 8, fontweight = "bold")
+        plt.gca().text(p.get_x() + p.get_width()/2, height/2, str(height), ha = "center", color = "white", fontsize = 8, fontweight = "bold", rotation = 90)
+
+# Function for add labls to non-stacked bar, but with special parameters for short columns
+# Define Function
+def add_bar_value_counts_spec():
+    # Creating a loop to put the value count of each genre count into the middle of its respective bar vertically in white font
+    for i, p in enumerate(plt.gca().patches):
+        height = p.get_height()
+        # For all bars but last two, show the label inside the bar with white font
+        if height < 10: 
+            plt.gca().text(p.get_x() + p.get_width()/2, height + 10, str(height), ha = "center", color = "black")
+        elif i < len(plt.gca().patches) - 2:
+            plt.gca().text(p.get_x() + p.get_width()/2, height/2, str(height), ha = "center", color = "white", rotation = 90)
+        else: 
+            # For the last two columns, put label above the bar in black font
+            plt.gca().text(p.get_x() + p.get_width()/2, height + 10, str(height), ha = "center", color = "black", rotation = 90)
+
+# Function to enumerate value counts for specific graphs
+# Define Function
+def enum_val_cnts(series, ax, color = "white", ha = "center", fontsize = 12, fontweight = 600):
+    # Plot the value counts of each genre inside the bar centered vertically in white font
+    # i = the index of each element in the series as an integer that starts at 0 and increments by 1 for each iteration 
+    # v = the value of each element in the series as the actual count & is a numeric value
+    # str(v) = converts the value of v to a string to be used in the bar plot
+    for i, v in enumerate(series):
+        ax.text(i, v/2, str(v), color = color, ha = ha, fontsize = fontsize, fontweight = fontweight)
